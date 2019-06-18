7b2fcb3a66888eb51c63729f1c5c9150a4b22d56
William Weisberger <bill.weisberger@gmail.com>
2019-06-18 16:56:29 -0600

    Completed homework assignment

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//interpolation is using template literals to stick variables into a written statement
+//concatenation is adding strings together
+
+//interpolation:
+let name = "Bill";
+console.log(`Hey, ${name}`);
+
+//concatenation
+name = "Frank"
+console.log("Hey, " + name);
+
+//DRY stands for Don't Repeat Yourself
+//It means any time you see yourself doing the same thing a few times
+//You should write code to automate it, and call that code.
+//We've learned aliases in bash, functions in JS, not sure what else.
+
+//Declaring a variable is using a 
